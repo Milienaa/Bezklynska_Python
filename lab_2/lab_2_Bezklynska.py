bfa1c51124354e4fd1a62f7ebb25a02daaa2991f
Miliena <bezklinskayamiliena@gmail.com>
2024-10-20 21:42:24 +0300

    second_comm

---
+++ lab_2/lab_2_Bezklynska.py
@@ -0,0 +1,27 @@
+#task 1
+print("Programming", "Essential", "in", sep="***", end="..." )
+print("Python")
+
+#task 2
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+#task 3
+print("I'm student")
+print('or')
+print('I\'m student')
+
+#task 4
+print('"I\'m"\n""learning""\n"""Python"""')
+
+#task 5
+print(int("500", 8))
+
+#task 6
+print(int("777", 16))
